f32d1379b07968b5e17580a8ea95af1ec937c054
Afshin Arani <afshin@arani.dev>
2023-02-09 14:07:55 +0330

    Remove the need for bitcoin full node
    This commit adds middleware that uses GWallet's
    existing electrum client to help RGS verify utxos
    while removing the need for bitcoin full node.

--- src/verifier.rs
+++ src/verifier.rs
@@ -1,11 +1,10 @@
+use hex;
 use std::convert::TryInto;
 
-use bitcoin::{BlockHash, TxOut};
-use bitcoin::blockdata::block::Block;
-use bitcoin::hashes::Hash;
+use bitcoin::{BlockHash, TxOut, Transaction};
+use bitcoin::consensus::deserialize;
 use lightning::chain;
 use lightning::chain::AccessError;
-use lightning_block_sync::{BlockData, BlockSource};
 use lightning_block_sync::http::BinaryResponse;
 use lightning_block_sync::rest::RestClient;
 
@@ -20,32 +19,42 @@ struct RestBinaryResponse(Vec<u8>);
 impl ChainVerifier {
 	pub(crate) fn new() -> Self {
 		ChainVerifier {
-			rest_client: RestClient::new(config::bitcoin_rest_endpoint()).unwrap(),
+			rest_client: RestClient::new(config::middleware_rest_endpoint()).unwrap(),
 		}
 	}
 
-	fn retrieve_block(&self, block_height: u32) -> Result<Block, AccessError> {
+	fn retrieve_tx(&self, block_height: u32, transaction_index: u32) -> Result<Transaction, AccessError> {
 		tokio::task::block_in_place(move || { tokio::runtime::Handle::current().block_on(async move {
-			let uri = format!("blockhashbyheight/{}.bin", block_height);
-			let block_hash_result =
+			let uri = format!("getTransaction/{}/{}", block_height, transaction_index);
+			let tx_result =
 				self.rest_client.request_resource::<BinaryResponse, RestBinaryResponse>(&uri).await;
-			let block_hash: Vec<u8> = block_hash_result.map_err(|error| {
-				eprintln!("Could't find block hash at height {}: {}", block_height, error.to_string());
+			let tx_hex_in_bytes: Vec<u8> = tx_result.map_err(|error| {
+				eprintln!("Could't find transaction at height {} and pos {}: {}", block_height, transaction_index, error.to_string());
 				AccessError::UnknownChain
 			})?.0;
-			let block_hash = BlockHash::from_slice(&block_hash).unwrap();
 
-			let block_result = self.rest_client.get_block(&block_hash).await;
-			match block_result {
-				Ok(BlockData::FullBlock(block)) => {
-					Ok(block)
-				},
-				Ok(_) => unreachable!(),
-				Err(error) => {
-					eprintln!("Couldn't retrieve block {}: {:?} ({})", block_height, error, block_hash);
-					Err(AccessError::UnknownChain)
-				}
-			}
+			let tx_hex_in_string =
+				String::from_utf8(tx_hex_in_bytes)
+					.map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
+					.unwrap();
+
+			let tx_bytes =
+				hex::decode(tx_hex_in_string)
+					.map_err(|error| {
+						eprintln!("Could't find transaction at height {} and pos {}: {}", block_height, transaction_index, error.to_string());
+						AccessError::UnknownChain
+					})
+					.unwrap();
+
+			let transaction =
+				deserialize::<Transaction>(tx_bytes.as_slice())
+					.map_err(|error| {
+						eprintln!("Could't find transaction at height {} and pos {}: {}", block_height, transaction_index, error.to_string());
+						AccessError::UnknownChain
+					})
+					.unwrap();
+
+			Ok(transaction)
 		}) })
 	}
 }
@@ -56,11 +65,8 @@ impl chain::Access for ChainVerifier {
 		let transaction_index = ((short_channel_id >> 2 * 8) & 0xffffff) as u32;
 		let output_index = (short_channel_id & 0xffff) as u16;
 
-		let block = self.retrieve_block(block_height)?;
-		let transaction = block.txdata.get(transaction_index as usize).ok_or_else(|| {
-			eprintln!("Transaction index {} out of bounds in block {} ({})", transaction_index, block_height, block.block_hash().to_string());
-			AccessError::UnknownTx
-		})?;
+		let transaction = self.retrieve_tx(block_height, transaction_index)?;
+
 		let output = transaction.output.get(output_index as usize).ok_or_else(|| {
 			eprintln!("Output index {} out of bounds in transaction {}", output_index, transaction.txid().to_string());
 			AccessError::UnknownTx
